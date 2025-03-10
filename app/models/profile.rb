b37fe855fd29bf6ab07ce096e0f7d683bd0693ab
dipaferdian <dipaferdian2@gmail.com>
2025-03-10 13:06:16 +0700

    new update

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,12 +4,4 @@ class Profile < ApplicationRecord
   belongs_to  :officer
 
   validates :status, presence: true, inclusion: { in: STATUS_TYPES }
-
-  def self.create_profile(officer:)
-    new_profile = Profile.new
-    new_profile.status = "good"
-    new_profile.officer_id = officer.id
-    new_profile.save
-    new_profile
-  end
 end
